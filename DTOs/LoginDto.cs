c775c017c5e09d9e41619a07f6219ad040e44085
Hebatallah Mujahed Kana <hebakana2004@gmail.com>
2026-06-24 21:17:24 +0300

    fix

--- DTOs/LoginDto.cs
+++ DTOs/LoginDto.cs
@@ -4,6 +4,6 @@
     {
         public string Email { get; set; } = string.Empty;// User's email address
 
-        public string Password { get; set; } = string.Empty;// User's password
+        public string Password { get; set; } = string.Empty;// User password
     }
 }
